2fd8456ef4a8d6f02e78f015d3247b3486a7fab5
beginner-x <739454011@qq.com>
2022-06-25 09:11:44 +0800

    8-Vue-toRef、toRefs、toRaw

--- vite-app/src/App.vue
+++ vite-app/src/App.vue
@@ -1,70 +1,55 @@
 <template>
   <div>
-    <div>{{message}}</div>
-    <div>{{message1}}</div>
-    <div>{{shallowReactiveTest}}</div>
-    <button @click="change1">1</button>
-    <button @click="change2">2</button>
+    <div><button @click="change">gaibian</button></div>
+    <div>{{state}}</div>
+
+    <div>'------foo'{{foo}}</div>
+    <div>'------bar'{{bar}}</div>
+    <button @click="change1">change1</button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { reactive, readonly, shallowReactive } from 'vue'
-
+import { toRef, reactive, toRefs, toRaw } from 'vue'
 /**
- * 1.异步赋值，通过push等方式进行添加，不能直接 = 赋值，否则会失去响应式
+ * 1.toRef
  */
-// 1.异赋值1
-let message = reactive<number[]>([]) // [] {}
-setTimeout(() => {
-  const arr = [1, 2, 3, 4]
-  // message = arr
-  message.push(...arr)
-}, 1000)
-
-// 2.异赋值2
-type O = {
-  list: number[]
+const obj = {
+  foo: 1,
+  bar: 1
 }
-let message1 = reactive<O>({
-  list: []
-})
-setTimeout(() => {
-  const arr = [1, 2, 3, 4]
-  message1.list = arr
-}, 1000)
+const state = toRef(obj, 'bar')
+const change = () => {
+  state.value++
 
-/**
- * 2.readonly 拷贝一份proxy对象，并将其设置为只读，不可修改
- */
-const person = reactive({
-  count: 1
-})
-let copy = readonly(person)
-// copy.count++
+  console.log('----原始对象', obj);
 
+  console.log('----引用对象', state);
+}
 /**
- * 3.shallowReactive 只能对浅层的数据响应式，如果是深层的数据只会改变值，不会改变视图
- * 注：挂载前的首次执行会进行更新
- * 值改变后，其它响应式数据改变更新视图，会引发该类型数据更新视图
+ * 2.toRefs 使解构出来的数据也为响应式
  */
-const shallowReactiveTest = shallowReactive({
-  test: 'xxx',
-  nav: {
-    bar: {
-      name: '我是谁'
-    }
-  }
+const obj1 = reactive({
+  foo: 1,
+  bar: 1
 })
+const { foo, bar } = toRefs(obj1)
 const change1 = () => {
-  shallowReactiveTest.test = '我被改了'
+  foo.value++
+  bar.value--
 }
-const change2 = () => {
-  shallowReactiveTest.nav.bar.name = '我被改了nav-bar-name'
-}
-// 挂载前执行会改变视图
-// change1()
-// change2()
+
+/**
+ * 3.toRaw 将响应式对象变成原始对象，可节省内存
+ */
+const obj2 = reactive({
+  foo: 1,
+  bar: 1
+})
+const raw = toRaw(obj2)
+console.log('响应式的', obj2);
+console.log('raw', raw);
+
 </script>
 
 <style>
