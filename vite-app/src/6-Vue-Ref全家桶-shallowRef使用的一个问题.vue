c6557b423b8f4a9f062ac80f0a30b9a65ab89c64
beginner-x <739454011@qq.com>
2022-06-25 08:55:15 +0800

    7-Vue-Reactive全家桶

---
+++ vite-app/src/6-Vue-Ref全家桶-shallowRef使用的一个问题.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    {{R}}
+    <button @click="Rfn">Rfn</button>
+    <button @click="changeMsg">change</button>
+    {{ message }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, shallowRef } from 'vue'
+/**
+ * shallowRef可以让对象不更新视图，
+ * 但ref定义的数据发生改变使视图更新时，会导致shallowRef定义对象的视图发生变化
+ */
+const R = ref('hello')
+const Rfn = () => {
+  R.value = 'dff'
+}
+const message = shallowRef({
+  foo: 'xx',
+  ber: 'lll'
+})
+const changeMsg = () => {
+  message.value.foo = 'change msg'
+  console.log(message);
+  
+}
+</script>
+
+<style>
+</style>
