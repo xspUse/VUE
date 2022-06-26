67ac32f8e270da268999feab5651db9b333a1b33
beginner-x <739454011@qq.com>
2022-06-26 22:47:48 +0800

    23-Vue-依赖注入provide、inject

---
+++ vite-app/src/components/23-Vue-依赖注入provide、inject-A.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="A">
+      <h1>我是A组件</h1>
+      <div>
+        {{data}}
+      </div>
+      <B></B>
+    </div>
+</template>
+
+<script setup lang="ts">
+import B from './B.vue'
+import { inject } from 'vue';
+let data = inject('flag')
+</script>
+
+<style lang="less">
+.A {
+  width: 250px;
+  height: 250px;
+  background: green;
+  color: #fff;
+}
+</style>
---
+++ vite-app/src/components/23-Vue-依赖注入provide、inject-B.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="B">
+      <h1>我是B组件</h1>
+      <button @click="changeFlag">change Flag</button>
+      <div>
+        {{data}}
+      </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { inject, Ref, ref } from 'vue';
+// inject第二个参数ref(false) 为兜底逻辑
+let data = inject<Ref<boolean>>('flag', ref(false))
+
+const changeFlag = () => {
+  data.value = true
+}
+</script>
+
+<style lang="less">
+.B {
+  width: 200px;
+  height: 200px;
+  background: blue;
+  color: #fff;
+}
+</style>
