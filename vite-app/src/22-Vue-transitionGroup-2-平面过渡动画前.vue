9cbef0599ae1c8251b3654717de9dde4b0cc089c
beginner-x <739454011@qq.com>
2022-06-26 21:57:25 +0800

    22-Vue-transitionGroup-2-列表的移动过渡动画、lodash工具库的安装

---
+++ vite-app/src/22-Vue-transitionGroup-2-平面过渡动画前.vue
@@ -0,0 +1,14 @@
+<template>
+    <Layout></Layout>
+</template>
+
+<script setup lang="ts">
+import Layout from './layout/index.vue'
+</script>
+
+<style lang="less">
+html,body,#app {
+  height: 100%;
+  overflow: hidden;
+}
+</style>
