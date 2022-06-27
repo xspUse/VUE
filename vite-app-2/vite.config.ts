464d28cb2480f6e10fb4fc4c35d38864794cf930
beginner-x <739454011@qq.com>
2022-06-27 11:57:02 +0800

    Vue-自动引入插件

--- vite-app-2/vite.config.ts
+++ vite-app-2/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx';
+// vite 版本 unplugin-auto-import/vite
+import AutoImport from 'unplugin-auto-import/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),vueJsx()]
+  plugins: [vue(),vueJsx(),AutoImport({
+    // 注入项
+    imports: ['vue'],
+    // 声明文件放置位置
+    dts:'src/auto-import.d.ts'
+  })]
 })
