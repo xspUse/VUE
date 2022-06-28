dc354749c0daab30a2cfb506924c60a9982a250a
beginner-x <739454011@qq.com>
2022-06-28 12:19:41 +0800

    27-Vue-自定义指令-方框拖拽案例

---
+++ vite-app-2/src/27-Vue-自定义指令-函数简写-App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <input v-model="value" type="text">
+      <A v-move="{background: value}"></A>
+    </div>
+</template>
+
+<script setup lang="ts">
+import A from './components/A.vue'
+import { Directive, DirectiveBinding, ref } from 'vue'
+const value = ref<string>('')
+type Dir = {
+  background: string
+}
+const vMove:Directive = (el:HTMLElement, binding:DirectiveBinding<Dir>) => {
+  el.style.background = binding.value.background
+}
+
+</script>
+
+<style lang="less">
+
+</style>
