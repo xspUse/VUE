98c3ccb498097540aa74c7da9f35906a386b0e57
beginner-x <739454011@qq.com>
2022-06-28 23:50:27 +0800

    32-Vue-详解scoped和样式穿透

--- vite-app-2/src/main.ts
+++ vite-app-2/src/main.ts
@@ -1,15 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 // element-plus引入
-// import ElementPlus from 'element-plus'
-// import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 // Antd引入 
-import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.css'
+// import Antd from 'ant-design-vue'
+// import 'ant-design-vue/dist/antd.css'
 const app = createApp(App)
 
-// app.use(ElementPlus)
+app.use(ElementPlus)
 
-app.use(Antd)
+// app.use(Antd)
 app.mount('#app')
