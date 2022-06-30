48474eacc44babe38742144ebcb378c1b073e887
beginner-x <739454011@qq.com>
2022-06-30 16:45:35 +0800

    5-novid-Map优化、版本兼容处理

--- novid/src/App.vue
+++ novid/src/App.vue
@@ -5,8 +5,7 @@
     class="box"
   >
     <div class="box-left"></div>
-    <div id="china" class="box-center">
-    </div>
+    <div id="china" class="box-center"></div>
     <div class="box-right"></div>
   </div>
 </template>
@@ -17,7 +16,7 @@ import { useStore } from "./stores";
 import { onMounted } from "vue";
 // import echarts from 'echarts' // echarts 4的引入方式
 import * as echarts from "echarts"; // echarts 5的引入方式
-import './assets/china'
+import "./assets/china";
 const store = useStore();
 
 store.getList();
@@ -36,55 +35,54 @@ onMounted(() => {
   ];
 
   charts.setOption({
-    backgroundColor: "black",
+    // backgroundColor: "black",
     geo: {
       map: "china",
       aspectScale: 0.8,
       layoutCenter: ["50%", "50%"],
       layoutSize: "120%",
       itemStyle: {
-        normal: {
-          areaColor: {
-            type: "linear-gradient",
-            x: 0,
-            y: 1200,
-            x2: 1000,
-            y2: 0,
-            colorStops: [
-              {
-                offset: 0,
-                color: "#152E6E", // 0% 处的颜色
-              },
-              {
-                offset: 1,
-                color: "#0673AD", // 50% 处的颜色
-              },
-            ],
-            global: true, // 缺省为 false
-          },
-          shadowColor: "#0f5d9d",
-          shadowOffsetX: 0,
-          shadowOffsetY: 15,
-          opacity: 0.5,
+        // normal: {
+        areaColor: {
+          type: "linear-gradient",
+          x: 0,
+          y: 1200,
+          x2: 1000,
+          y2: 0,
+          colorStops: [
+            {
+              offset: 0,
+              color: "#152E6E", // 0% 处的颜色
+            },
+            {
+              offset: 1,
+              color: "#0673AD", // 50% 处的颜色
+            },
+          ],
+          global: true, // 缺省为 false
         },
-        emphasis: {
-          areaColor: "#0f5d9d",
-        },
+        shadowColor: "#0f5d9d",
+        shadowOffsetX: 0,
+        shadowOffsetY: 15,
+        opacity: 0.5,
+        // },
       },
-
+      emphasis: {
+        areaColor: "#0f5d9d",
+      },
       regions: [
         {
           name: "南海诸岛",
           itemStyle: {
             areaColor: "rgba(0, 10, 52, 1)",
             borderColor: "rgba(0, 10, 52, 1)",
-            normal: {
-              opacity: 0,
-              label: {
-                show: false,
-                color: "#009cc9",
-              },
+            // normal: {
+            opacity: 0,
+            label: {
+              show: false,
+              color: "#009cc9",
             },
+            // },
           },
           label: {
             show: false,
@@ -98,7 +96,7 @@ onMounted(() => {
       {
         type: "map",
         selectedMode: "multiple",
-        mapType: "china",
+        map: "china",
         aspectScale: 0.8,
         layoutCenter: ["50%", "50%"], //地图位置
         layoutSize: "120%",
@@ -110,50 +108,48 @@ onMounted(() => {
           max: 2,
         },
         label: {
-          show: false,
+          // 省名显示
+          show: true,
           color: "#FFFFFF",
-          fontSize: 16,
+          fontSize: 12,
         },
         itemStyle: {
-          normal: {
-            areaColor: "#0c3653",
-            borderColor: "#1cccff",
-            borderWidth: 1.8,
-          },
-          emphasis: {
-            areaColor: "#56b1da",
-            label: {
-              show: false,
-              color: "#fff",
-            },
+          // normal: {
+          areaColor: "#0c3653",
+          borderColor: "#1cccff",
+          borderWidth: 1.8,
+          // },
+        },
+        emphasis: {
+          areaColor: "#56b1da",
+          label: {
+            show: true, // 移上去字体显示
+            color: "#fff",
           },
         },
         data: data,
       },
       {
-        name: "Top 5",
         type: "scatter",
         coordinateSystem: "geo",
         //   symbol: 'image://http://ssq168.shupf.cn/data/biaoji.png',
         // symbolSize: [30,120],
         // symbolOffset:[0, '-40%'] ,
+        // 数字角标，气泡样式
+        symbol: "pin",
+        symbolSize: [45, 45],
         label: {
-          normal: {
-            show: false,
-          },
+          // 数字显示
+          // normal: {
+          show: true,
+          // },
         },
         itemStyle: {
-          normal: {
-            color: "#D8BC37", //标志颜色
-          },
+          // normal: {
+          color: "#D8BC37", //标志颜色
+          // },
         },
         data: data,
-        showEffectOn: "render",
-        rippleEffect: {
-          brushType: "stroke",
-        },
-        hoverAnimation: true,
-        zlevel: 1,
       },
     ],
   });
@@ -177,13 +173,13 @@ body,
   display: flex;
   overflow: hidden;
   &-left {
-    width: 400px;
+    width: 300px;
   }
   &-center {
     flex: 1;
   }
   &-right {
-    width: 400px;
+    width: 300px;
   }
 }
 </style>
