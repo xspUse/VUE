e296e7e20204cc1129e33da18360492dd7d2502b
beginner-x <739454011@qq.com>
2022-06-30 11:09:57 +0800

    3-novid-初始化前端

--- novid/src/App.vue
+++ novid/src/App.vue
@@ -1,13 +1,45 @@
 <template>
-    <div> 
-      
-    </div>
+  <div :style="{background: `url(${bg}) no-repeat`}" style="background-size:cover;" class="box">
+    <div class="box-left"></div>
+    <div class="box-center"></div>
+    <div class="box-right"></div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import bg from "./assets/1.jpg"
+import { useStore } from './stores'
+
+const store = useStore()
+
+store.getList()
 
 </script>
 
-<style lang="less" scoped> 
-
+<style lang="less">
+* {
+  padding: 0;
+  margin: 0;
+}
+// scoped要去除
+html,
+body,
+#app {
+  height: 100%;
+  overflow: hidden;
+}
+.box {
+  height: 100%;
+  display: flex;
+  overflow: hidden;
+  &-left {
+    width: 400px;
+  }
+  &-center {
+    flex: 1;
+  }
+  &-right {
+    width: 400px;
+  }
+}
 </style>
---
+++ novid/src/stores/index.ts
@@ -0,0 +1,15 @@
+import { defineStore } from 'pinia'
+import { getApiList } from '../server'
+export const useStore = defineStore({
+  id: 'counter',
+  state: () => ({
+    list: {}
+  }),
+  actions: {
+    async getList () {
+      const result = await getApiList()
+      console.log(result);
+      
+    }
+  }
+})
